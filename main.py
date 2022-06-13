8e064e452c33de6eb6744d23b5dbfe8442f0cbb2
msabeni <kemimsabeni@gmail.com>
2022-06-13 13:14:24 +0300

    using random.choice() to have the computer randomly select between the actions and printing the chosen computer and user actions

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random
 
 user_action = input("Enter a choice (rock, paper, scissors): ")
+
+possible_actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
+
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
